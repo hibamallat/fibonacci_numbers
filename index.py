e863ff4c781768bcf232d5003cbd709ffbf4dfff
hibamallat <hibamallat040@gmail.com>
2024-11-07 00:29:38 +0200

    Clean the code

--- index.py
+++ index.py
@@ -3,34 +3,34 @@ Printing fibonacci sequence
 """
 
 try:
-    fibonacci_terms = int(input("Enter n to generate a Fibonacci sequence: "))
-    fibonacci_count = 0 
+    Fibonacci_Terms = int(input("Enter n to generate a Fibonacci sequence: "))
+
     # Set the first two numbers in the Fibonacci sequence
-    base1 = 0
-    base2 = 1
+    Base1 = 0
+    Base2 = 1
 
-    base1_copy = base1
-    base2_copy = base2
+    Base1_Copy = Base1
+    Base2_Copy = Base2
 
-    if fibonacci_terms <= 0:
+    if Fibonacci_Terms <= 0:
         print("INVALID:It should have a positive value")
 
-    elif fibonacci_terms == 1:
-        print(base1, end = ' ')
+    elif Fibonacci_Terms == 1:
+        print(Base1, end = ' ')
 
     else:
-        print(base1, base2, end = ' ')
+        print(Base1, Base2, end = ' ')
 
-    for i in range(fibonacci_terms - 2): #I set the range to n - 2 because 0 and 1 are always printed initially.
+    for i in range(Fibonacci_Terms - 2): #I set the range to n - 2 because 0 and 1 are always printed initially
+
         #Determine the next number by summing the two previous numbers
-        fibonacci_sum = base1_copy + base2_copy
+        Fibonacci_Sum = Base1_Copy + Base2_Copy
 
-        if fibonacci_count < fibonacci_terms :
-            print(fibonacci_sum, end = ' ')
-            fibonacci_count += 1
-            # Update the copy of base1 and base2 for the next iteration
-            base1_copy = base2_copy
-            base2_copy = fibonacci_sum
+        print(Fibonacci_Sum, end = ' ')
+
+        # Update the copy of base1 and base2 for the next iteration
+        Base1_Copy = Base2_Copy
+        Base2_Copy = Fibonacci_Sum
 
 except ValueError:
     print("INVALID:Enter a number only")
